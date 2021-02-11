66446e93e210f9e5199c98487cc3f93445f19c5e
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2021-02-11 17:27:13 +0100

    add errors

---
+++ build/errors/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NotAuthorizedError = exports.NotFoundError = exports.CustomError = exports.RequestValidationError = exports.DatabaseConnectionError = exports.BadRequestError = void 0;
+var bad_request_error_1 = require("./bad-request-error");
+Object.defineProperty(exports, "BadRequestError", { enumerable: true, get: function () { return bad_request_error_1.BadRequestError; } });
+var database_connection_error_1 = require("./database-connection-error");
+Object.defineProperty(exports, "DatabaseConnectionError", { enumerable: true, get: function () { return database_connection_error_1.DatabaseConnectionError; } });
+var request_validation_error_1 = require("./request-validation-error");
+Object.defineProperty(exports, "RequestValidationError", { enumerable: true, get: function () { return request_validation_error_1.RequestValidationError; } });
+var custom_error_1 = require("./custom-error");
+Object.defineProperty(exports, "CustomError", { enumerable: true, get: function () { return custom_error_1.CustomError; } });
+var not_found_error_1 = require("./not-found-error");
+Object.defineProperty(exports, "NotFoundError", { enumerable: true, get: function () { return not_found_error_1.NotFoundError; } });
+var not_authorized_err_1 = require("./not-authorized-err");
+Object.defineProperty(exports, "NotAuthorizedError", { enumerable: true, get: function () { return not_authorized_err_1.NotAuthorizedError; } });
